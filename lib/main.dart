54694535f114cbc2874af01fb2404632e64c1f58
Md._Mubin <mumah786@gmail.com>
2023-05-27 16:08:58 +0000

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Dashboard',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -23,19 +24,52 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 60),
+                ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+                  //leading: Icon(Icons.dashboard),
+                  title: Text(
+                    "Hello, Md. Mubin",
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                  subtitle: Text(
+                    "Good Morning!",
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Colors.white54,
+                        ),
+                  ),
+                  trailing: const CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage('lib/myphoto.png'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
